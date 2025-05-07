dac66f5e702ec3cccb93f20217b741c820380408
Thomas ROUSSELIN <t.rousselin@mtarget.fr>
2025-05-07 13:07:15 +0200

    first commit

---
+++ src/test/java/com/devattom/springcore/SpringcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devattom.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
